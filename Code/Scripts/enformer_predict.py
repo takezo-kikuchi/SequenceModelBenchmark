8ccffd975bdb0fde44eaec6b020fed675fc860df
takezo-kikuchi <tkiku1414@gmail.com>
2024-05-27 14:47:13 +0900

    edited enformer_predict.py

--- Code/Scripts/enformer_predict.py
+++ Code/Scripts/enformer_predict.py
@@ -32,7 +32,15 @@ parser.add_argument("--track-dict-path")
 parser.add_argument("--verbose", action="store_true")
 args = parser.parse_args()
 
-
+# Set memory growth
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+        
 # as per https://stackoverflow.com/a/59571639
 def get_gpu_memory():
     command = "nvidia-smi --query-gpu=memory.free --format=csv"
